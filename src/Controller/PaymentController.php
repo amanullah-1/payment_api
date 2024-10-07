aa38ead8a2df85b6b31ff29db0e5d7e4371bef0a
Md Aman Ullah <amanu092@gmail.com>
2024-10-07 22:50:39 +0800

    make the response json pretty

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,7 +2,6 @@
 // src/Controller/PaymentController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +19,7 @@ class PaymentController
     /**
      * @Route("/app/example/{system}", name="process_payment", methods={"POST"})
      */
-    public function processPayment(Request $request, $system): JsonResponse
+    public function processPayment(Request $request, $system): Response
     {
         // Get input parameters from the request
         $amount = $request->get('amount');
@@ -37,18 +36,8 @@ class PaymentController
             $system, $amount, $currency, $cardNumber, $expYear, $expMonth, $cvv, $paymentBrand, $paymentType
         );
 
-        
-        // echo "<pre />";
-        // print_r($response);
-        // $response = json_encode($response,JSON_PRETTY_PRINT);
-        // header('Content-Type: application/json');
-        $response = new JsonResponse($response);
-        $response->setEncodingOptions(JSON_UNESCAPED_UNICODE);
-        $response->setEncodingOptions(JSON_PRETTY_PRINT );
-        
-        // Use the JSON_PRETTY_PRINT 
-        // $response->setEncodingOptions( $response->getEncodingOptions() | JSON_PRETTY_PRINT );
-        // Return the unified response
-        return $response;
+        $response = new Response($response);
+
+        return new Response($response, 200, ['Content-Type' => 'application/json']);
     }
 }
